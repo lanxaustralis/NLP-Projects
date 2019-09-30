ce891a02bb9a08fbd67690fdf27086706a788915
Yüşa Ömer <Altntop>
2019-09-30 23:59:34 +0300

    Made Outputs Ready for Submission
    Cleaned some code, made a friendly console

--- naive-bow.jl
+++ naive-bow.jl
@@ -7,13 +7,11 @@ Pkg.add("Random")
 import Knet: accuracy
 
 
-VOCAB_THRESHOLD = 1  # represents most common words, which are not class specific -> Increasing this constant is inefficient in terms of computational perform, however may a slight posiitive effect on the accuracy
-VOCAB_SIZE = 30000     # gets most common words in order
-SENTENCE_SIZE = 200 # increasing this constant, increases the accuracy value, in most cases.
+VOCAB_THRESHOLD = 20  # represents most common words, which are not class specific -> Increasing this constant is inefficient in terms of computational perform, however may a slight posiitive effect on the accuracy
+VOCAB_SIZE = 50000     # gets most common words in order
+SENTENCE_SIZE = 250 # increasing this constant, increases the accuracy value, in most cases.
 TR_TS_SIZE = 25000
 UNKNOWN = "<unk>"
-
-# change this for a better approach
 
 using Knet: accuracy, param ,Param, @diff, grad, params, KnetArray
 using Random
@@ -21,8 +19,12 @@ using Random
 # thanks https://www.rosettacode.org/wiki/Strip_a_set_of_characters_from_a_string#Julia for their kind advice
 stripChar = (s, r) -> replace(s, Regex("[$r]") => " ")
 
+# change this directory wrt test environment
 dir = "/home/minuteman/academics/'19 Fall/NLP/Project-Repo/NLP-Projects/aclImdb_v1/aclImdb/"
 
+println("---Welcome---")
+println("|- The constants of the program as follows -|\n")
+println((Vocabulary_Size=VOCAB_SIZE,Train_Test_Size=TR_TS_SIZE,Unknown_Tag=UNKNOWN,Sentence_Size=SENTENCE_SIZE,Vocabulary_Commonality_Threshold=VOCAB_THRESHOLD))
 function readandprep(dir)
     sentences = []
 
@@ -164,7 +166,10 @@ function obtainvocab(sentences)
 
 end
 
+println()
+print("Preparation for training data ->")
 @time train_word_tag = readandprep(dir * "/train") # sentences and tags stored here
+print("Preparation for vocabulary ->")
 @time pos_freq_dict,
     neg_freq_dict,
     vocab_freq_dict,
@@ -176,7 +181,7 @@ end
     pos_latter,
     neg_latter,
     voc_latter = obtainvocab(train_word_tag)
-
+print("Preparation for test data ->")
 @time test_word_tag = readandprep(dir * "/test")
 
 # Please comment out here to check total probability of words within a set (i.e. vocabulary)
@@ -227,51 +232,48 @@ end
 # println("The similarity ratio of preprocessed and raw comments (decrease VOCAB_THRESHOLD to work on more similar sets) -> $(same*1.0/TR_TS_SIZE)")
 # println("----------------------------------------")
 
-# function pred(sentence)
-#     p_vector = prob_p, prob_n = (1.0, 1.0)
-#     #println("Sentence is -> "*string(sentence))
-#     for word in sentence
-#         con_vector = pos_p, neg_p = calcwithLaplace(word)
-#         #println("Calculated vector for "*string(word)*" -> "*string(con_vector))
-#         p_vector = p_vector .* con_vector
-#         #println("Updated prob vector -> "*string(p_vector))
-#     end
-#     #println("Result p vector -> "*string(p_vector))
-#     return p_vector[1] > p_vector[2] # true for positive, false for negative
-# end
+function pred(sentence)
+    p_vector = prob_p, prob_n = (1.0, 1.0)
+    #println("Sentence is -> "*string(sentence))
+    for word in sentence
+        con_vector = pos_p, neg_p = calcwithLaplace(word)
+        #println("Calculated vector for "*string(word)*" -> "*string(con_vector))
+        p_vector = p_vector .* con_vector
+        #println("Updated prob vector -> "*string(p_vector))
+    end
+    #println("Result p vector -> "*string(p_vector))
+    return p_vector[1] > p_vector[2] # true for positive, false for negative
+end
 
-# function calcwithLaplace(word)
-#     global pos_word_num
-#     global neg_word_num
-#     global total_word_num
-#
-#     global vocab_freq_dict
-#     global pos_freq_dict
-#     global neg_freq_dict
-#
-# # since the floating numbers becomes to low, it is applied the original format of the formula
-#     if !haskey(vocab_freq_dict, word)
-#         #println(word*" is a new word!!!")
-#         return (1.0 / pos_word_num, 1.0 / neg_word_num) .* total_word_num # it is already added the size of the vocab
-#     end
-#     return (pos_freq_dict[word], neg_freq_dict[word]) ./ vocab_freq_dict[word] # Laplace smoothing was set before
-# end
-#
-# function predall(comment_tag_set)
-#     correct = 0
-#     #print("Valid ones ")
-#     for i = 1:TR_TS_SIZE
-#         sentence, tag = comment_tag_set[i]
-#         #print(sentence*string(tag))
-#         valid = pred(sentence) == tagclassifier(tag)
-#         if valid
-#             correct += 1
-#             #print(string(i)*" ")
-#         end
-#     end
-#     #println("")
-#     return correct * 1.0 / TR_TS_SIZE
-# end
+function calcwithLaplace(word)
+    global pos_word_num
+    global neg_word_num
+    global total_word_num
+
+    global vocab_freq_dict
+    global pos_freq_dict
+    global neg_freq_dict
+
+# since the floating numbers becomes to low, it is applied the original format of the formula
+    if !haskey(vocab_freq_dict, word)
+        #println(word*" is a new word!!!")
+        return (1.0 / pos_word_num, 1.0 / neg_word_num) .* total_word_num # it is already added the size of the vocab
+    end
+    return (pos_freq_dict[word], neg_freq_dict[word]) ./ vocab_freq_dict[word] # Laplace smoothing was set before
+end
+
+function predall(comment_tag_set)
+    correct = 0
+    #print("Valid ones ")
+    for i = 1:TR_TS_SIZE
+        sentence, tag = comment_tag_set[i]
+        valid = pred(sentence) == tagclassifier(tag)
+        if valid
+            correct += 1
+        end
+    end
+    return correct * 1.0 / TR_TS_SIZE
+end
 
 function tagclassifier(tag)
     return parse(Int64, tag) > 5 # tag > 5 are positive comments which result true, vice versa
@@ -280,70 +282,85 @@ end
 
 # Pure Way - w/o explicit traininig
 
-# Comment out above code
+# Comment out below code
+println()
+print("Preprocessing train data ->")
+preped_trn = @time prepwrtvocab(train_word_tag, vocab_freq_dict)
+print("Preprocessing test data ->")
+preped_tst = @time prepwrtvocab(test_word_tag, vocab_freq_dict)
 
-# preped_trn = @time prepwrtvocab(train_word_tag, vocab_freq_dict)
-# preped_tst = @time prepwrtvocab(test_word_tag, vocab_freq_dict)
-#
-# rng = MersenneTwister(12345)
-# shuffle!(rng, preped_trn)
-# shuffle!(rng, preped_tst)
-#
-# @time acc_trn = predall(preped_trn)
-# @time acc_tst = predall(preped_tst)
-#
-# println("Accuracy for train -> " * string(acc_trn))
-# println("Accuracy for test -> " * string(acc_tst))
+rng = MersenneTwister(12345)
+shuffle!(rng, preped_trn)
+shuffle!(rng, preped_tst)
+
+println()
+print("Prediction for train->")
+@time acc_trn = predall(preped_trn)
+print("Prediction for test->")
+@time acc_tst = predall(preped_tst)
+
+println()
+println("Accuracy for train -> " * string(acc_trn))
+println("Accuracy for test -> " * string(acc_tst))
 
 
+# Active Learning
+
+# SGD seems not working for this case, therefore above code recomended for the performance monitoring
+
+# But the matrix approach seems more efficent in terms of computational performance
+# For the following strategy, one Weight matrix which includes conditional probabilities of
+# each word with respect to given classes (size of 2*VOCAB_SIZE)
+# Each word tokenized as Prof. Yuret recomended, which stored in a word -> id vocab
+# which makes matrix operation easier as the sentence array turnd into id array and tag
 
 # From now on, the strategy is changed to the on learning process
 
 # First check container whether updates are correct or not
-function testStrategyChange()
-    global voc_latter
-
-    # for positive class update
-    global pos_freq_dict
-    global pos_latter
-
-    mismatch = 0
-
-    for (word,freq) in pos_freq_dict
-        if !haskey(voc_latter,word)
-            println(word*" is not registered on the newer dictionary")
-            mismatch+=1
-            continue
-        end
-        index = voc_latter[word]
-        if freq!=pos_latter[index]
-            println(word*" -> values of freq for this word does not match -- positive class")
-            mismatch+=1
-            #return
-        end
-    end
-
-    # for negative class update
-    global neg_freq_dict
-    global neg_latter
-
-    trial = VOCAB_SIZE
-    for (word,freq) in neg_freq_dict
-        if !haskey(voc_latter,word)
-            println(word*" is not registered on the newer dictionary")
-            mismatch+=1
-            continue
-        end
-        index = voc_latter[word]
-        if freq!=neg_latter[index]
-            println(word*" -> values of freq for this word does not match -- negative class")
-            mismatch+=1
-            #return
-        end
-    end
-
-    println("Total mismatch number over vocabulary size "*string(VOCAB_SIZE)*" -> "*string(mismatch))
-end
+# function testStrategyChange()
+#     global voc_latter
+#
+#     # for positive class update
+#     global pos_freq_dict
+#     global pos_latter
+#
+#     mismatch = 0
+#
+#     for (word,freq) in pos_freq_dict
+#         if !haskey(voc_latter,word)
+#             println(word*" is not registered on the newer dictionary")
+#             mismatch+=1
+#             continue
+#         end
+#         index = voc_latter[word]
+#         if freq!=pos_latter[index]
+#             println(word*" -> values of freq for this word does not match -- positive class")
+#             mismatch+=1
+#             #return
+#         end
+#     end
+#
+#     # for negative class update
+#     global neg_freq_dict
+#     global neg_latter
+#
+#     trial = VOCAB_SIZE
+#     for (word,freq) in neg_freq_dict
+#         if !haskey(voc_latter,word)
+#             println(word*" is not registered on the newer dictionary")
+#             mismatch+=1
+#             continue
+#         end
+#         index = voc_latter[word]
+#         if freq!=neg_latter[index]
+#             println(word*" -> values of freq for this word does not match -- negative class")
+#             mismatch+=1
+#             #return
+#         end
+#     end
+#
+#     println("Total mismatch number over vocabulary size "*string(VOCAB_SIZE)*" -> "*string(mismatch))
+# end
 
 # Run this code to see whether there is any mismatch between both strategies
 #testStrategyChange()
@@ -369,25 +386,29 @@ function prepwrtvocab(sentence_arr)
 
     return sent_id_arr # id tag array
 end
-
+println("----------------------------------------")
+println("Matrix Strategy Results")
+println()
 # Contianers of new format
+print("Preprocessing train data for matrix strategy ->")
 preped_trn_latter = @time prepwrtvocab(train_word_tag)
+print("Preprocessing test data for matrix strategy ->")
 preped_tst_latter = @time prepwrtvocab(test_word_tag)
+println()
 
-using Knet: param
-Knet.param(dims...) = Param(Array(0.01f0 * randn(Float32, dims...)))
+# using Knet: param
+# Knet.param(dims...) = Param(Array(0.01f0 * randn(Float32, dims...)))
 
-# W = Knet.param(2,length(voc_latter)) # pos to 1, neg to 2
 W = Param(Array(vcat(pos_latter,neg_latter)))
 
 pred(words) = prod(W[:,words], dims=2)
 
-function loss(words, tag)
-    scores = pred(words)
-    logprobs = scores .- log(sum(exp.(scores)))
-    -logprobs[tag]
-end
-
+#  No need for this if we do not perform an optimization algorithm
+# function loss(words, tag)
+#     scores = pred(words)
+#     logprobs = scores .- log(sum(exp.(scores)))
+#     -logprobs[tag]
+# end
 
 
 function Knet.accuracy(data)
@@ -395,19 +416,23 @@ function Knet.accuracy(data)
 end
 
 
-function train(; nepochs = 10, lr = 0.01)
-    trn = preped_trn_latter
+function train(; nepochs = 3, lr = 0.1) # epoch as
     for epoch in 1:nepochs
-        shuffle!(trn)
-        for (x,y) in trn
+        shuffle!(preped_trn_latter)
+        # SGD seems ineffective
+        for (x,y) in preped_trn_latter
             tag = y ? 1 : 2 # if it is true, the it is in class 1 which is positive class
             ∇loss = @diff loss(x,tag)
             for p in params(∇loss)
                 p .= p - lr * grad(∇loss, p)
             end
         end
-        println((epoch = epoch, trn = accuracy(trn)))
+        println((epoch = epoch, trn = accuracy(preped_trn_latter)))
     end
 end
 
-#train()
+train()
+println()
+print("Result for test -> ")
+println(accuracy(preped_tst_latter))
+println("---End---")
